9dfc778f98636b2fd3d36d20f3ab96838d0884fa
basekeet <alexandr.arhipochkin@gmail.com>
2025-03-15 13:28:37 +0300

    add model renderer

--- sample-engine/main.cpp
+++ sample-engine/main.cpp
@@ -6,65 +6,92 @@
 #include <assimp/postprocess.h>
 #include <iostream>
 #include <random>
+#include <vector>
+#include <queue>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+
+typedef unsigned int ui32;
+typedef float f32;
+typedef size_t szt;
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+
 class sceneRenderer {
     const aiScene* scene;
-    unsigned int numVertices;
-    float* vertices;
-    unsigned int numIndices;
-    unsigned int* indices;
-    bool once = false;
-    unsigned int VBO, VAO, EBO;
 
+    ui32 numMeshes;
+
+    ui32* numVertices;
+    f32** vertices;
+
+    ui32* numIndices;
+    ui32** indices;
+   
+    ui32* VBO;
+    ui32* VAO;
+    ui32* EBO;
+
+    Shader* shader;
 public:
-    sceneRenderer(const aiScene* scene) {
+    sceneRenderer(const aiScene* scene, Shader* shader) {
+        this->shader = shader;
+
         this->scene = scene;
-    }
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<float> dis(0.0f, 1.0f);
 
-    void drawMesh(size_t ind) {
-        if (!once) {
-            std::random_device rd;
-            std::mt19937 gen(rd());
-            std::uniform_real_distribution<float> dis(0.0f, 1.0f);
+        numMeshes = scene->mNumMeshes;
 
-            numVertices = scene->mMeshes[ind]->mNumVertices;
-            vertices = new float[scene->mMeshes[ind]->mNumVertices * 6];
-            for (size_t i = 0; i < numVertices; i++) {
-                vertices[i * 6] = scene->mMeshes[ind]->mVertices[i][0] / 2;
-                vertices[i * 6 + 1] = scene->mMeshes[ind]->mVertices[i][1] / 2;
-                vertices[i * 6 + 2] = scene->mMeshes[ind]->mVertices[i][2] / 2;
-                vertices[i * 6 + 3] = dis(gen);
-                vertices[i * 6 + 4] = dis(gen);
-                vertices[i * 6 + 5] = dis(gen);
+        numVertices = new ui32[numMeshes];
+        vertices = new f32 * [numMeshes];
+        numIndices = new ui32[numMeshes];
+        indices = new ui32 * [numMeshes];
+
+        VBO = new ui32[numMeshes];
+        VAO = new ui32[numMeshes];
+        EBO = new ui32[numMeshes];
+
+        glGenVertexArrays(numMeshes, VAO);
+        glGenBuffers(numMeshes, VBO);
+        glGenBuffers(numMeshes, EBO);
+
+        for (size_t i = 0; i < numMeshes; i++) {
+            numVertices[i] = scene->mMeshes[i]->mNumVertices;
+            vertices[i] = new float[scene->mMeshes[i]->mNumVertices * 6];
+            for (size_t j = 0; j < numVertices[i]; j++) {
+                vertices[i][j * 6] = scene->mMeshes[i]->mVertices[j][0];
+                vertices[i][j * 6 + 1] = scene->mMeshes[i]->mVertices[j][1];
+                vertices[i][j * 6 + 2] = scene->mMeshes[i]->mVertices[j][2];
+                vertices[i][j * 6 + 3] = dis(gen);
+                vertices[i][j * 6 + 4] = dis(gen);
+                vertices[i][j * 6 + 5] = dis(gen);
             }
 
-            numIndices = scene->mMeshes[ind]->mNumFaces;
-            indices = new unsigned int[numIndices * 3];
-            for (size_t i = 0; i < numIndices; i++) {
-                indices[i * 3 + 2] = scene->mMeshes[ind]->mFaces[i].mIndices[0];
-                indices[i * 3 + 1] = scene->mMeshes[ind]->mFaces[i].mIndices[1];
-                indices[i * 3 + 0] = scene->mMeshes[ind]->mFaces[i].mIndices[2];
+            numIndices[i] = scene->mMeshes[i]->mNumFaces;
+            indices[i] = new unsigned int[numIndices[i] * 3];
+            for (size_t j = 0; j < numIndices[i]; j++) {
+                indices[i][j * 3 + 2] = scene->mMeshes[i]->mFaces[j].mIndices[0];
+                indices[i][j * 3 + 1] = scene->mMeshes[i]->mFaces[j].mIndices[1];
+                indices[i][j * 3 + 0] = scene->mMeshes[i]->mFaces[j].mIndices[2];
             }
-        
-            
-            glGenVertexArrays(1, &VAO);
-            glGenBuffers(1, &VBO);
-            glGenBuffers(1, &EBO);
 
-            glBindVertexArray(VAO);
+            glBindVertexArray(VAO[i]);
 
-            glBindBuffer(GL_ARRAY_BUFFER, VBO);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(float) * numVertices * 6, vertices, GL_STATIC_DRAW);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO[i]);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(float) * numVertices[i] * 6, vertices[i], GL_STATIC_DRAW);
 
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int ) * numIndices * 3, indices, GL_STATIC_DRAW);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[i]);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * numIndices[i] * 3, indices[i], GL_STATIC_DRAW);
 
             // position attribute
             glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
@@ -72,27 +99,53 @@ public:
             // color attribute
             glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
             glEnableVertexAttribArray(1);
-            once = true;
         }
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, numIndices * 3, GL_UNSIGNED_INT, 0);
+    }
+
+    void drawMeshRec(aiNode* node, glm::mat4 trans) {
+        aiMatrix4x4 lt = node->mTransformation;
+        lt.Transpose();
+        glm::mat4 localTrans;
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                localTrans[i][j] = lt[i][j];
+            }
+        }
+        
+        trans = trans * localTrans;
+        unsigned int transformLoc = glGetUniformLocation(shader->ID, "transform");
+
+        for (size_t i = 0; i < node->mNumMeshes; i++) {
+            
+            glUniformMatrix4fv(transformLoc, 1, GL_FALSE, (float*)&trans);
+           
+            glBindVertexArray(VAO[node->mMeshes[i]]);
+            glDrawElements(GL_TRIANGLES, numIndices[node->mMeshes[i]] * 3, GL_UNSIGNED_INT, 0);
+        }
+
+        for (size_t i = 0; i < node->mNumChildren; i++) {
+            drawMeshRec(node->mChildren[i], trans);
+        }
+    }
+
+    void drawMesh() {
+
+        shader->use();
+        drawMeshRec(scene->mRootNode, glm::scale(glm::rotate(glm::mat4(1.0f), (float)glfwGetTime() / 10, glm::vec3(0.0f, 1.0f, 0.0f)), glm::vec3(0.5f, 0.5f, 0.5f)));
+        //drawMeshRec(scene->mRootNode, aiMatrix4x4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0 , 0, 0, 1));
     }
 
 };
 
 int main()
 {
-    const aiScene* scene = aiImportFile("resources/Barbarian.fbx", aiProcessPreset_TargetRealtime_MaxQuality);
-    sceneRenderer rendere(scene);
-    
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
 
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
@@ -112,47 +165,21 @@ int main()
 
     Shader ourShader("resources/shader.vs", "resources/shader.fs"); 
 
-    float vertices[] = {
-        // positions         // colors
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
-    };
-
-    std::cout << sizeof(vertices);
-
-
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-
+    const aiScene* scene = aiImportFile("resources/Barbarian.fbx", aiProcessPreset_TargetRealtime_MaxQuality);
+    sceneRenderer rendere(scene, &ourShader);
+    //rendere.drawMesh();
+    glEnable(GL_DEPTH_TEST);
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
-
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        ourShader.use();
-        rendere.drawMesh(0);
+        rendere.drawMesh();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
 
     glfwTerminate();
     return 0;
